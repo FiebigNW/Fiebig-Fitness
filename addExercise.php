ae6b1c1cd737c9842dc988559061c4fec36bbd51
Nicholas Fiebig <160694108+FiebigNW@users.noreply.github.com>
2024-11-30 16:37:57 -0600

    Update addStrengthExercise.php

--- addExercise.php
+++ addExercise.php
@@ -11,13 +11,14 @@ $accountID = $_SESSION['AccountID'];
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $exerciseName = $_POST['exerciseName'];
     $exerciseDate = $_POST['exerciseDate'];  
+    $exerciseType = 'Strength';
     
     try {
         require_once "dbConnection.php";
        
-        $query = "INSERT INTO Exercises(AccountID, ExerciseName, Exercise_Date) VALUES (?, ?, ?);";
+        $query = "INSERT INTO Exercises(AccountID, ExerciseName, Exercise_Date, Exercise_Type) VALUES (?, ?, ?, ?);";
         $stmt = $pdo->prepare($query);
-        $stmt->execute([$accountID, $exerciseName, $exerciseDate]);
+        $stmt->execute([$accountID, $exerciseName, $exerciseDate, $exerciseType]);
 
         $pdo = null;
         $stmt = null;
